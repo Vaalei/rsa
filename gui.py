1f2a12b8362ef354843f87fb71ed7cd734bb34a2
BuildTools <unconfigured@null.spigotmc.org>
2021-10-22 20:46:23 +0200

    background, .gitignore

--- gui.py
+++ gui.py
@@ -1,111 +1,154 @@
-
 from tkinter import *
 from tkinter import ttk
-from rsaModules import getCryptoPrimes, getE, encrypt, decrypt, removeLetters, int2Text, text2Int
+from tkinter import scrolledtext as st
+from rsaModules import getCryptoPrimes, getKeys, encrypt, decrypt, removeLetters, int2Text, text2Int, modSize
+from webbrowser import BackgroundBrowser, open_new
+
+
+
 
 
 # Generate RSA keys
-p, q = getCryptoPrimes(256)
-n = p*q
-pk, sk, mod = getE(p,q)
+def genRSA(bits):
+    p, q = getCryptoPrimes(bits)
+    pk, sk, mod = getKeys(p,q)
+    return pk, sk, mod
 
-print("E:", pk, "\nN:", mod, "\n")
+def rgb(rgb):
+    # translates an rgb tuple of int to a tkinter friendly color code
+    return "#%02x%02x%02x" % rgb  
 
-# 
-def changeEncrypt(value):
+
+def changeEncrypt():
+    value = encryptInputField.get("1.0", END)
     if chosenType.get() == "Integer":
         try:
             value = int(value)
         except:
             value = removeLetters(value)
         output = encrypt(value, pk, mod)
-        encryptOutputVar.set(output)
 
     elif chosenType.get() == "Text":
         value = text2Int(value)
         output = encrypt(value, pk, mod)
-        encryptOutputVar.set(output)
 
-def changeDecrypt(value):
+    encryptOutputField.delete("1.0","end")
+    encryptOutputField.insert("1.0", output)
+
+def changeDecrypt():
+    value = decryptInputField.get("1.0", END)
     try:
         value = int(value)
     except:
         value = removeLetters(value)
     if chosenType.get() == "Text":
         output = decrypt(value, sk, mod)
-        output = int2Text(output, mod)
-        decryptOutputVar.set(output)
+        output = int2Text(output, modSize(mod))
 
     elif chosenType.get() == "Integer":
         output = decrypt(value, sk, mod)
-        decryptOutputVar.set(output)
+
+    decryptOutputField.delete("1.0","end")
+    decryptOutputField.insert("1.0", output)
 
 
+BGCOLOUR = rgb((18,18,18))
 
 
-# 
 root = Tk()
-root.geometry("500x200")
+#root.geometry("550x230")
 root.title("RSA Kryptering @Vaalei")
+root.configure(
+    background=BGCOLOUR
+)
 
 frm = ttk.Frame(root, padding=20)
 frm.grid()
 
+
 style = ttk.Style()
-style.configure("BW.TLabel", foreground="black", background="white")
+style.configure("BW.TLabel", 
+    foreground="white", 
+    background=BGCOLOUR,
+    font= "helvetica 10 bold"
+    )
 
 
-"""
-frm.grid(row=0, column=0, sticky="NESW")
-frm.grid_rowconfigure(0, weight=1)
-frm.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
-"""
+
 
 # ENCRYPTION
 # Input
-ttk.Label(frm, text="Encrypt", font="Helvetica 18 bold", padding=0).grid(column=1, row=0)
-encryptInputText = ttk.Label(frm, text="Input:", padding=0).grid(column=0, row=1)
+encryptTitle = ttk.Label(root, style="BW.TLabel", text="Encrypt", font="Helvetica 18 bold", padding=0)
+encryptTitle.grid(column=1, row=0)
+
+encryptInputText = ttk.Label(root, style="BW.TLabel", text="Input:", padding=0)
+encryptInputText.grid(column=0, row=1)
 encryptInputVar = StringVar()
-encryptInputField = ttk.Entry(frm, textvariable=encryptInputVar).grid(column=1, row=1)
+encryptInputField = Text(root, width=30, height=10)
+encryptInputField.grid(column=1, row=1)
 
 # Output
-encryptOutputText = ttk.Label(frm, text="Encrypted:", padding=0).grid(column=0, row=2)
+encryptOutputText = ttk.Label(root, style="BW.TLabel", text="Encrypted:", padding=0)
+encryptOutputText.grid(column=0, row=2)
 encryptOutputVar = StringVar()
-encryptOutputField = ttk.Entry(frm, textvariable=encryptOutputVar).grid(column=1, row=2)
+encryptOutputField = Text(root, width=30, height=10)
+encryptOutputField.grid(column=1, row=2)
 
-calculateButton = ttk.Button(frm, text="Calculate", padding=0, command=lambda: changeEncrypt(encryptInputVar.get())).grid(column=1, row=3)
+calculateButton = ttk.Button(root, style="BW.TLabel", text="Calculate", padding=0, command=lambda: changeEncrypt())
+calculateButton.grid(column=1, row=3)
 
 # END OF ENCRYPTION
 
-# PADDING
-ttk.Label(frm, text="         ").grid(column=2,row=0)
-ttk.Label(frm, text=""). grid(column=0, row=4)
+
 
 
 # DECRYPTION
 # Input
-ttk.Label(frm, text="Decrypt", font="Helvetica 18 bold", padding=0).grid(column=4, row=0)
-decryptInputText = ttk.Label(frm, text="Input:", padding=0).grid(column=3, row=1)
+decryptTitle = ttk.Label(root, style="BW.TLabel", text="Decrypt", font="Helvetica 18 bold", padding=0)
+decryptTitle.grid(column=4, row=0)
+decryptInputText = ttk.Label(root, style="BW.TLabel", text="Input:", padding=0)
+decryptInputText.grid(column=3, row=1)
 decryptInputVar = StringVar()
-eecryptInputField = ttk.Entry(frm, textvariable=decryptInputVar).grid(column=4, row=1)
+decryptInputField = Text(root, width=30, height=10)
+decryptInputField.grid(column=4, row=1)
 
 # Output
-decryptOutputText = ttk.Label(frm, text="Encrypted:", padding=0).grid(column=3, row=2)
+decryptOutputText = ttk.Label(root, style="BW.TLabel", text="Encrypted:", padding=40)
+decryptOutputText.grid(column=3, row=2)
 decryptOutputVar = StringVar()
-decryptOutputField = ttk.Entry(frm, textvariable=decryptOutputVar).grid(column=4, row=2)
+decryptOutputField = Text(root, width=30, height=10)
+decryptOutputField.grid(column=4, row=2)
 
-calculateButton = ttk.Button(frm, text="Calculate", padding=0, command=lambda: changeDecrypt(decryptInputVar.get())).grid(column=4, row=3)
+calculateButton = ttk.Button(root, style="BW.TLabel", text="Calculate", padding=0, command=lambda: changeDecrypt(), cursor="hand2")
+calculateButton.grid(column=4, row=3)
 
-# END OF DECTRYPTION
+# END OF DECRYPTION
+
 
 # Options
-ttk.Label(frm, text="Type of encryption:").grid(column=0, row=6)
+ttk.Label(root, style="BW.TLabel", text="Type of encryption:", font="Halvetica 10 bold").grid(column=0, row=6)
 chosenType = StringVar()
 chosenType.set("Integer")
-chooseType = ttk.OptionMenu(frm, chosenType, "Choose Type Here","Integer", "Text").grid(column=1, row=6)
+chooseType = ttk.OptionMenu(root, chosenType, "Choose Encryption Type Here", "Integer", "Text")
+chooseType.grid(column=1, row=6)
+chooseType.configure(
+    width=30
+)
 
 
+# PADDING
+#ttk.Label(root, style="BW.TLabel", text="", padding=0).grid(column=2,row=99) # Between different convertors
+ttk.Label(root, style="BW.TLabel", text="").grid(column=0, row=4) # Between 
+ttk.Label(root, style="BW.TLabel", text="").grid(column=0, row=98) # Before Github link
+ttk.Label(root, style="BW.TLabel", text="     ").grid(column=99,row=100)
 
-root.mainloop()
+
+# Github
+github = Label(root, text="Github.com @Vaalei", fg="blue", cursor="hand2", font="Helvetica 11", bg=BGCOLOUR)
+github.grid(column=0,row=99)
+github.bind("<Button-1>", lambda e: open_new("https://github.com/Vaalei"))
+
+
+if __name__ == "__main__":
+    pk, sk, mod = genRSA(256)
+    root.mainloop()
--- rsaModules.py
+++ rsaModules.py
@@ -2,12 +2,13 @@ from numbers import Number
 from random import choice
 from math import sqrt
 from time import time
-from Crypto.Util import number
+try:
+    from Crypto.Util import number # pip install pycryptodome
+except:
+    print('module Crypto not found \nuse "pip install pycryptodome" to install it')
 from functools import reduce
-# pip install pycryptodome
 
 
-primesUpToHundred = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 # https://www.geeksforgeeks.org/prime-factor/
 def primeFactors(n):
@@ -24,56 +25,19 @@ def primeFactors(n):
     return list
 
 
-def getNumberFromFile(name):
-    # Behövs inte
-    file = open(name, "r")
-
-    list = []
-
-    for col in file:
-        col = col.replace("\n", "")
-        list.append(int(col))
-
-    file.close()
-    return list
-
-
 def getCryptoPrimes(bit):
     num1 = number.getPrime(bit)
     num2 = number.getPrime(bit)
     return num1, num2
 
 
-def getE(p,q):
+def getKeys(p,q):
     phi, mod = (p - 1) * (q - 1), p * q
     if mod < 65537:
         return (3, inv(3, phi), mod)
     else:
         return (65537, inv(65537, phi), mod)
     
-
-
-
-def getLowestE(listOfEs, subtractedPrimes):
-    for i in listOfEs:
-        temp = 0
-        while temp*i < subtractedPrimes:
-            temp+=1
-        if temp*i == subtractedPrimes:
-            return i
-
-
-def getD(e, f):
-    d = 0 
-    while True:
-        d+=1
-        if e * d % f == 1:
-            return d 
-
-        # If there are high primes involved, you may want to print how much it has computed
-        if d % 10**8 == 0:
-            print(str(d/10**8)+"*10^8")
-        
 
 def decrypt(hiddenMessage, sk, mod):
     decryptedMessage = pow(hiddenMessage, sk, mod)
@@ -82,22 +46,6 @@ def decrypt(hiddenMessage, sk, mod):
 def encrypt(message, pk, mod):
     return pow(message, pk, mod)
 
-
-def getNumInput(shownText=""):
-    string = input(shownText)
-    if string == "": 
-        return ""
-    if string == "exit":
-        return "exit"
-    list = []
-    for i in string:
-        try:
-            int(i)
-            list.append(i)
-        except:
-            pass
-    final = int("".join(list))
-    return final
 
 def removeLetters(string):
     list = []
@@ -108,18 +56,7 @@ def removeLetters(string):
         except:
             pass
     final = int("".join(list))
-    return final
-
-
-def getFastD(e, phi):
-    d_old = 0; r_old = phi
-    d_new = 1; r_new = e
-    while r_new > 0:
-        a = r_old // r_new
-        (d_old, d_new) = (d_new, d_old - a * d_new)
-        (r_old, r_new) = (r_new, r_old - a * r_new)
-    return d_old % phi if r_old == 1 else None
-
+    return final 
 
 
 def inv(p, q):
@@ -141,6 +78,9 @@ def inv(p, q):
         t += q
     return t
 
+# credit : http://jhafranco.com/2012/01/29/rsa-implementation-in-python/ 
+# for both text2Int & int2Text
+ 
 def text2Int(text):
     """Convert a text string into an integer"""
     return reduce(lambda x, y : (x << 8) + y, map(ord, text))
